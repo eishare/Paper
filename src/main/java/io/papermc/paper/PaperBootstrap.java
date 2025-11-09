1a05a774fb0ceaf47d92ca7c0a4ab7e8c2c24e68
eishare <easonhunt@protonmail.com>
2025-11-10 02:37:11 +0800

    Update PaperBootstrap.java

--- src/main/java/io/papermc/paper/PaperBootstrap.java
+++ src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -150,8 +150,7 @@ public class PaperBootstrap {
                 "listen_port": 443,
                 "users": [{ "uuid": "UUID", "password": "eishare2025" }],
                 "congestion_control": "bbr",
-                "udp_relay_mode": "native",
-                "heartbeat": "10s",
+                "udp_relay_mode": "quic"
                 "tls": {
                   "enabled": true,
                   "alpn": ["h3"],
